5a84164e845d968c1d96580fee8df17236e9b38a
Maddy <184935886+Maddy-bee@users.noreply.github.com>
2025-02-19 14:04:05 +0000

    added comments for clarity

--- fetch/src/app/Hero/Hero.tsx
+++ fetch/src/app/Hero/Hero.tsx
@@ -1,9 +1,17 @@
 import React, { useState } from 'react';
 import styles from './Hero.module.css';
 
+//Create an interface for the search bar props
+// The component should accept a single argument of type string (the city).
+//The component does not return anything (void means it has no return value).
 interface SearchBarProps {
     onSearch: (city: string) => void;
 }
+
+// create search bar component 
+//set initial state as empty
+// take in an input value of city 
+//create event for when "fetch" button is clicked 
 
 const SearchBar: React.FC<SearchBarProps> = ({ onSearch }) => {
     const [city, setCity] = useState('');
