bd055973f891d98591a5c1ef6644f6b09dd1ca1d
Owen <98043758+Oweshbin@users.noreply.github.com>
2025-02-19 13:59:22 +0000

    Added the Footer component

---
+++ fetch/src/app/Footer/footer.tsx
@@ -0,0 +1,11 @@
+import styles from "./footer.module.css";
+
+const Footer = () => {
+  return (
+    <div className={styles.footer}>
+      <p className={styles.footerText}>© Team MOSS</p>
+    </div>
+  );
+};
+
+export default Footer;
